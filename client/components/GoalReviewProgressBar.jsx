a4d91298dc91d229422da3cf464cddc238aa7827
Austen Talbot <austentalbot@gmail.com>
2015-06-01 22:00:30 -0700

    Added progress selector

--- client/components/GoalReviewProgressBar.jsx
+++ client/components/GoalReviewProgressBar.jsx
@@ -3,24 +3,37 @@ var R = React.createElement;
 var GoalStore = require('../stores/GoalStore.js');
 var AppDispatcher = require('../dispatcher/AppDispatcher.js');
 
-var maxLength = 200;
-
+var width = 350;
 var GoalReviewProgressBar = module.exports = React.createClass({
   getInitialState: function() {
     return {
+      x: .5
     }
   },
   onProgressClick: function(e) {
     var rect = this.getDOMNode().getBoundingClientRect();
     var score = parseInt(((e.pageX - rect.left)/rect.width)*100);
     console.log('score', score);
+    this.setState({ x: score/100 })
   },
   render: function() {
     var that = this;
+
+    // xcxc need to update positioning
+    var dotStyle = {
+      left: this.state.x * width + 'px'
+    };
+    console.log('style', dotStyle);
+    var dot = R('div', {
+      className: 'goal-review-progress-bar-indicator',
+      style: dotStyle
+    }, ' ');
     
     return R('div', {
       className: 'goal-review-progress-bar',
-      onClick: this.onProgressClick
+      style: { width: width },
+      onClick: this.onProgressClick,
+      children: [dot]
     });
   }
 });
